d05652bd24b517ed027b994e4418e38b83782c9c
devToGit <nnos@mail.ru>
2020-02-10 18:39:27 +0300

    2.1 -Учебный проект: валидируй, как никогда не валидировал

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import PropTypes from "prop-types";
 import WelcomeScreen from "../welcome-screen/welcome-screen.jsx";
 
+
 const App = (props) => {
-  // eslint-disable-next-line react/prop-types
+
   const {errorsCount} = props;
 
   return (
@@ -10,5 +12,8 @@ const App = (props) => {
   );
 };
 
+App.propTypes = {
+  errorsCount: PropTypes.number.isRequired,
+};
 
 export default App;
